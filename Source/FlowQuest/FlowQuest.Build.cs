79b1df86477e93c4112c4e56b0c29635732af5ed
Moth Doctor <krzysiek.justynski@gmail.com>
2021-03-27 16:30:18 +0100

    4.25

--- Source/FlowQuest/FlowQuest.Build.cs
+++ Source/FlowQuest/FlowQuest.Build.cs
@@ -16,7 +16,6 @@ public class FlowQuest : ModuleRules
             "AIModule",
             "Core",
 			"CoreUObject",
-            "DeveloperSettings",
 			"Engine",
             "GameplayTags",
             "Slate",
