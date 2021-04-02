5bba2578accc6db686cd58d5271b7b03f4abb0ac
Moth Doctor <krzysiek.justynski@gmail.com>
2021-04-02 11:03:16 +0200

    today I learned that clearing value of _DEPREACATED is redundant

--- Source/FlowQuest/Private/Flow/Nodes/FlowNode_SetInteractionState.cpp
+++ Source/FlowQuest/Private/Flow/Nodes/FlowNode_SetInteractionState.cpp
@@ -20,7 +20,6 @@ void UFlowNode_SetInteractionState::PostLoad()
 	if (IdentityTag_DEPRECATED.IsValid())
 	{
 		IdentityTags = FGameplayTagContainer(IdentityTag_DEPRECATED);
-		IdentityTag_DEPRECATED = FGameplayTag();
 	}
 }
 
