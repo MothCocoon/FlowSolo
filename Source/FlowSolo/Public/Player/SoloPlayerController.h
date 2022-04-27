6fcfe9285a3c346a34a2900528c9024f117af149
Moth Doctor <krzysiek.justynski@gmail.com>
2022-04-27 22:58:40 +0200

    nativized Pawn input

--- Source/FlowSolo/Public/Player/SoloPlayerController.h
+++ Source/FlowSolo/Public/Player/SoloPlayerController.h
@@ -24,13 +24,31 @@ public:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
 	UInputAction* InteractionInput;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* MoveForwardInput;
 	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* MoveRightInput;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* CameraPitchInput;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* CameraYawInput;
+
 private:
 	TWeakObjectPtr<class USoloInputComponent> SoloInputComponent;
 	
 public:
 	virtual void SetupInputComponent() override;
 	void SetExplorationContext() const;
+
+	void MoveForward(const UInputAction* Action) const;
+	void MoveRight(const UInputAction* Action) const;
+
+	void AddPitch(const UInputAction* Action);
+	void AddYaw(const UInputAction* Action);
 
 	virtual void PlayerTick(float DeltaTime) override;
 
