04946403e5e64cf3ddeec5cfbf5fa7d948c1fe05
Moth Doctor <krzysiek.justynski@gmail.com>
2022-05-08 22:54:31 +0200

    Minimal Save System logic, just enough for testing purposes

---
+++ Source/FlowSolo/Public/Core/SaveSubsystem.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "FlowSubsystem.h"
+#include "SaveSubsystem.generated.h"
+
+/**
+ * Minimal logic, just enough for testing purposes
+ * WORK IN PROGRESS
+ */
+UCLASS()
+class USaveSubsystem final : public UFlowSubsystem, public FSelfRegisteringExec
+{
+	GENERATED_BODY()
+
+	USaveSubsystem();
+	
+public:
+	static FString CheckpointSlotName;
+
+	// FSelfRegisteringExec
+	virtual bool Exec(UWorld* InWorld, const TCHAR* Cmd, FOutputDevice& Ar) override;
+
+	void SaveGame();
+
+	UFUNCTION(Exec, Category = "SaveSubsystem")
+	void LoadGame();
+};
