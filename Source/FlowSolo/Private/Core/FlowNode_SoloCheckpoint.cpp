8982a8dd544ba14f41c8c983b9eaaec60d761cfc
Moth Doctor <krzysiek.justynski@gmail.com>
2022-05-08 23:08:19 +0200

    Minimal Save System logic, just enough for testing purposes

---
+++ Source/FlowSolo/Private/Core/FlowNode_SoloCheckpoint.cpp
@@ -0,0 +1,29 @@
+﻿#include "Core/FlowNode_SoloCheckpoint.h"
+#include "FlowSubsystem.h"
+#include "Core/SaveSubsystem.h"
+
+UFlowNode_SoloCheckpoint::UFlowNode_SoloCheckpoint(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+#if WITH_EDITOR
+	Category = TEXT("Utils");
+#endif
+}
+
+void UFlowNode_SoloCheckpoint::ExecuteInput(const FName& PinName)
+{
+	if (GetWorld())
+	{
+		if (USaveSubsystem* SaveSubsystem = GetWorld()->GetGameInstance()->GetSubsystem<USaveSubsystem>())
+		{
+			SaveSubsystem->SaveGame();
+		}
+	}
+
+	TriggerFirstOutput(true);
+}
+
+void UFlowNode_SoloCheckpoint::OnLoad_Implementation()
+{
+	TriggerFirstOutput(true);
+}
