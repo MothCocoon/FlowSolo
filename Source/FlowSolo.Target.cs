cca3b69dc12305df08627a110808ac766ebc74e1
FoolsTheoryDev <krzysiek.justynski@gmail.com>
2024-03-20 20:22:30 +0100

    UE 5.4 version of project

--- Source/FlowSolo.Target.cs
+++ Source/FlowSolo.Target.cs
@@ -1,16 +1,17 @@
+// Copyright https://github.com/MothCocoon/FlowGraph/graphs/contributors
+
 using UnrealBuildTool;
 
 public class FlowSoloTarget : TargetRules
 {
-	public FlowSoloTarget(TargetInfo Target) : base(Target)
+	public FlowSoloTarget(TargetInfo target) : base(target)
 	{
 		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange(new[]
 		{ 
 			"FlowSolo"
 		});
-
-        DefaultBuildSettings = BuildSettingsVersion.V4;
     }
 }
--- Source/FlowSoloEditor.Target.cs
+++ Source/FlowSoloEditor.Target.cs
@@ -1,17 +1,18 @@
+// Copyright https://github.com/MothCocoon/FlowGraph/graphs/contributors
+
 using UnrealBuildTool;
 
 public class FlowSoloEditorTarget : TargetRules
 {
-	public FlowSoloEditorTarget(TargetInfo Target) : base(Target)
+	public FlowSoloEditorTarget(TargetInfo target) : base(target)
 	{
 		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange(new[]
 		{ 
 			"FlowSolo"
 		});
-
-        DefaultBuildSettings = BuildSettingsVersion.V4;
     }
 }
