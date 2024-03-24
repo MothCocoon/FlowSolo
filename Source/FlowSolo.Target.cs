c07197b21ad63087d8a1638d88e99a8fccbb79d7
FoolsTheoryDev <krzysiek.justynski@gmail.com>
2024-03-24 19:03:48 +0100

    Update FlowSolo.Target.cs

--- Source/FlowSolo.Target.cs
+++ Source/FlowSolo.Target.cs
@@ -8,6 +8,7 @@ public class FlowSoloTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange(new[]
 		{ 
